4e98cbf9e887a8f6138c0ec1029f1c4477aa6030
Achille kouassi <patriceachillekouassin@gmail.com>
2023-02-07 19:29:25 +0100

    first commit

---
+++ exercice2.php
@@ -0,0 +1,27 @@
+<?php
+  if (isset($_POST['name']) && isset($_POST['age'])) {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    echo "Hi $name. You are $age years old.";
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form  method="post">
+  <label for="name">Nom:</label>
+  <input type="text" id="name" name="name">
+  <br><br>
+  <label for="age">Age:</label>
+  <input type="text" id="age" name="age">
+  <br><br>
+  <input type="submit" value="Envoyer">
+</form>
+</body>
+</html>
---
+++ exercice3.php
@@ -0,0 +1,16 @@
+<?php
+  function myFunction($value) {
+    try {
+      if (!filter_var($value, FILTER_VALIDATE_INT)) {
+        throw new Exception("La valeur n'est pas un entier valide");
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    } finally {
+      echo "\nValidation complete\n";
+    }
+  }
+
+  $value = 2.5;
+  myFunction($value);
+?>
